560e32054550b491eac6f9a183074bdf1569ea0e
Oybek <oybekjonxayrullayevich@gmail.com>
2022-07-04 13:26:54 +0500

    masala 3

--- index.php
+++ index.php
@@ -10,13 +10,28 @@
 // }
 
 //masala 2
-$son3 = -22;
-if ($son3>0) {
- $son4 = $son3+1;
-  echo $son4;
+// $son3 = -22;
+// if ($son3>0) {
+//  $son4 = $son3+1;
+//   echo $son4;
+// }
+// else{
+//   $son5 = $son3-2;
+//   echo $son5;
+// }
+
+// masala 3
+$son = -99;
+if ($son>0) {
+  $son++;
+  echo $son;
 }
-else{
-  $son5 = $son3-2;
-  echo $son5;
+elseif ($son<0) {
+$son2=$son-2;
+echo $son2;
+}
+elseif ($son==0) {
+  $son=10;
+  echo $son;
 }
 ?>
