2f2625ad1fb0030cfe9c3639445d6e534c3f9e87
Oybek <oybekjonxayrullayevich@gmail.com>
2022-07-04 13:23:40 +0500

    masala 2

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php 
-$son1=22;
-if ($son1>0) {
- $son2 = $son1+1;
-  echo $son2;
+//masala 1
+// $son1=22;
+// if ($son1>0) {
+//  $son2 = $son1+1;
+//   echo $son2;
+// }
+// else{
+//   echo $son1;
+// }
+
+//masala 2
+$son3 = -22;
+if ($son3>0) {
+ $son4 = $son3+1;
+  echo $son4;
 }
 else{
-  echo $son1;
+  $son5 = $son3-2;
+  echo $son5;
 }
 ?>
