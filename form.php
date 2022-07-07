a62a9293ee959b2f66ad28177eed459dc98b4122
Oybek <oybekjonxayrullayevich@gmail.com>
2022-07-07 18:00:34 +0500

    aloqa modulini yaratish

--- form.php
+++ form.php
@@ -7,13 +7,22 @@
   <title>Document</title>
 </head>
 <body>
-  <?php 
-  echo $_POST['name'],$_POST['email'];
-  ?>
-  <form action="form.php" method="post">
-    <input type="text" name="name">
-    <input type="email" name="email" id="">
-    <button type="submit" name="submit">Yuborish</button>
+ 
+  <form>
+    <input type="text" name="soz" placeholder="soz"><br><br>
+    <input type="text" name="avtor" placeholder="avtor"><br>
+    <input type="date" name="vaqt">
+    <input type="submit" name="s1" value="kiritish"/>
   </form>
+<?php
+if (isset($_GET["s1"])) {
+$con = mysqli_connect("localhost","root","","web9");
+$s=$_GET["soz"];
+$m=$_GET["avtor"];
+$v=$_GET["vaqt"];
+$sql = "insert into sozlar(soz,muallif,vaqt) values('{$s}','{$m}','{$v}')";
+$r = mysqli_query($con,$sql);
+}
+?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,55 +1,24 @@
-<?php 
-//masala 1
-// $son1=22;
-// if ($son1>0) {
-//  $son2 = $son1+1;
-//   echo $son2;
-// }
-// else{
-//   echo $son1;
-// }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div class="conatiner">
+<div class="col-4">
+  <li>
+    <a href="?m=xb">Xabar yuborish</a>
+  </li>
+  <li>
+    <a href="?m=faq">FAQ</a>
+  </li>
+</div>
+<div class="col-8">
 
-//masala 2
-// $son3 = -22;
-// if ($son3>0) {
-//  $son4 = $son3+1;
-//   echo $son4;
-// }
-// else{
-//   $son5 = $son3-2;
-//   echo $son5;
-// }
-
-// masala 3
-// $son = -99;
-// if ($son>0) {
-//   $son++;
-//   echo $son;
-// }
-// elseif ($son<0) {
-// $son2=$son-2;
-// echo $son2;
-// }
-// elseif ($son==0) {
-//   $son=10;
-//   echo $son;
-// }
-
-//masala 4
-
-$son1 = 9;
-$son2=8;
-$son3 = 0;
-if ($son1>0 && $son2<0 && $son3<0 || $son1<0 && $son2>0 && $son3<0 ||  $son1<0 && $son2<0 && $son3>0   ) {
-echo "bitta musbat son bor";
-}
-elseif ( $son1>0 && $son2>0 && $son3<0  ||  $son1<0 && $son2>0 && $son3>0  ||   $son1>0 && $son2<0 && $son3>0 ) {
-echo "ikkita musbat son bor";
-}
-elseif ( $son1>0 && $son2>0 && $son3>0 ) {
-  echo "hammasi musbat sonlar";
-}
-else{
-  echo "musbat son yuq "; 
-}
-?>
+</div>
+  </div>
+</body>
+</html>
