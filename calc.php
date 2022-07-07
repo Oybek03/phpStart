bdd2e42937e6bc074f561586cb3bc71ce3bb46c8
Oybek <oybekjonxayrullayevich@gmail.com>
2022-07-07 19:02:25 +0500

    calculator app

---
+++ calc.php
@@ -0,0 +1,40 @@
+<?php 
+
+?>
+<html>
+  <head>
+    <title>Calculator</title>
+  </head>
+  <table border="1">
+<tr>
+  <td colspan="4"><input type="text"></td>
+</tr>
+<tr>
+  <td><input type="submit" name="submit" value="7"></td>
+  <td><input type="submit" name="submit" value="8"></td>
+  <td><input type="submit" name="submit" value="9"></td>
+  <td><input type="submit" name="op" value="/"></td>
+</tr>
+
+<tr>
+  <td><input type="submit" name="submit" value="4"></td>
+  <td><input type="submit" name="submit" value="5"></td>
+  <td><input type="submit" name="submit" value="6"></td>
+  <td><input type="submit" name="op" value="+"></td>
+</tr>
+
+<tr>
+  <td><input type="submit" name="submit" value="1"></td>
+  <td><input type="submit" name="submit" value="2"></td>
+  <td><input type="submit" name="submit" value="3"></td>
+  <td><input type="submit" name="op" value="-"></td>
+</tr>
+
+<tr>
+  <td><input type="submit" name="submit" value="0"></td>
+  <td><input type="submit" name="submit" value="C"></td>
+  <td><input type="submit" name="equals" value="="></td>
+  <td><input type="submit" name="op" value="X"></td>
+</tr>
+  </table>
+</html>
