b2ff50dec90e92a043b3b99c8455b352bc4c522f
Oybek <oybekjonxayrullayevich@gmail.com>
2022-07-07 21:38:01 +0500

    styled

--- calc.php
+++ calc.php
@@ -2,18 +2,24 @@
 $num="";
 $operator="";
 $result="";
-$cooke_name="";
-$cooke_value="";
+$cooke_name1="num";
+$cooke_value1="";
+$cooke_name2="op";
+$cooke_value2="";
+
 ?>
 <html>
   <head>
     <title>Calculator</title>
   </head>
-  <table border="1">
-<tr>
-  <td colspan="4"><input type="text"></td>
-</tr>
-<tr>
+  <body>
+    <form action="#" method="post">
+
+      <table border="1">
+        <tr>
+          <td colspan="4"><input type="text" name="display"></td>
+        </tr>
+        <tr>
   <td><input type="submit" name="submit" value="7"></td>
   <td><input type="submit" name="submit" value="8"></td>
   <td><input type="submit" name="submit" value="9"></td>
@@ -40,5 +46,7 @@ $cooke_value="";
   <td><input type="submit" name="equals" value="="></td>
   <td><input type="submit" name="op" value="X"></td>
 </tr>
-  </table>
+</table>
+ </form>
+</body>
 </html>
