a6cc232a23c3298760b8b8f5db60d379496b389b
Oybek <oybekjonxayrullayevich@gmail.com>
2022-07-19 14:59:01 +0500

    add

--- test/connect.php
+++ test/connect.php
@@ -1,16 +1,19 @@
 <?php
 $name = $_POST["name"];
 $firstname = $_POST["firstname"];
-$conn = new mysqli('localhost','root','','test');
-if ($conn->connect_error) {
-  die('Connection Feild : ' .$conn->connect_error);
-}
 
-else{
-  $stmt = $conn->prepare("insert into registration(Ism,Familya) values(?,?)");
-  $stmt->bind_param("sssssi",$name,$firstname);
-  $stmt->execute();
-  echo "registration sacs...";
+
+$conn = new mysqli('localhost','root','','test');
+if($conn->connect_error){
+  echo "$conn->connect_error";
+  die("Connection Failed : ". $conn->connect_error);
+} else {
+  $stmt = $conn->prepare("insert into registration(Ism, Familya) 
+  values(?, ?)");
+  $stmt->bind_param($name, $firstname);
+  $execval = $stmt->execute();
+  echo $execval;
+  echo "Registration successfully...";
   $stmt->close();
   $conn->close();
 }
